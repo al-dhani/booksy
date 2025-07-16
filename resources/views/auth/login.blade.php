aadd9f45d99bd4c87f4b6c8c16482425e8af67b3
al-dhani <Alif>
2025-07-16 09:44:41 +0700

    tampilan login nya

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,18 +1,15 @@
 <x-guest-layout>
-    <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
     <form method="POST" action="{{ route('login') }}">
         @csrf
 
-        <!-- Email Address -->
         <div>
             <x-input-label for="email" :value="__('Email')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
-        <!-- Password -->
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
 
@@ -24,7 +21,6 @@
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
-        <!-- Remember Me -->
         <div class="block mt-4">
             <label for="remember_me" class="inline-flex items-center">
                 <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
@@ -32,26 +28,26 @@
             </label>
         </div>
 
+        <div class="flex items-center justify-between mt-4 w-full">
 
-        <div class="flex items-center justify-between flex-wrap gap-4 mt-4">
-    <div class="flex items-center space-x-4">
-        @if (Route::has('password.request'))
-            <a href="{{ route('password.request') }}" class="text-sm text-gray-600 hover:text-gray-900 underline">
-                Forgot your password?
-            </a>
-        @endif
+            @if (Route::has('password.request'))
+                <a href="{{ route('password.request') }}" class="text-sm text-gray-600 hover:text-gray-900 underline whitespace-nowrap">
+                    Lupa Password?
+                </a>
+            @endif
 
-        <div class="flex items-center justify-between flex-wrap gap-4 mt-4">
-    <div class="flex items-center space-x-4">
-        @if (Route::has('register'))
-            <a href="{{ route('register') }}" class="text-sm text-gray-600 hover:text-gray-900 underline">
-                Belum punya akun? Ayo buat sekarang
-            </a>
-        @endif
+            <div class="flex items-center gap-x-4">
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}" class="text-sm text-gray-600 hover:text-gray-900 underline whitespace-nowrap">
+                        Belum punya akun?
+                    </a>
+                @endif
+            </div>
 
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
+                <x-primary-button class="py-1 px-2 text-sm">
+                    {{ __('LOG IN') }}
+                </x-primary-button>
+            </div>
         </div>
     </form>
 </x-guest-layout>
